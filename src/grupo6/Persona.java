7873e868bbc9aafc4d846c63c379aab317382091
Santiago Ross <santidaross@outlook.com>
2020-08-25 13:34:01 -0300

    Subida inicial
    Se sube el proyecto con los puntos 1 y 2 ya resueltos (Clase Persona con encapsulamiento).

---
+++ src/grupo6/Persona.java
@@ -0,0 +1,31 @@
+package grupo6;
+
+public class Persona {
+
+	private int dni;
+	private String nombre;
+	private String apellido;
+	
+	public Persona() {
+		
+	}
+	public int getDni() {
+		return dni;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public String getApellido() {
+		return apellido;
+	}
+	public void setDni(int dni) {
+		this.dni = dni;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+	
+}
